1fa0b7484f44b2f00269e85ee8c4696f19418ea0
Marc-Andre Piche <marc-andre.piche@umontreal.ca>
2017-12-22 22:02:50 -0500

    update Merge

--- WordDistance.java
+++ WordDistance.java
@@ -2,87 +2,82 @@ import java.util.Arrays;
 
 /**
  * Created by Marc-André Piché|783722 on 2017-12-22.
- *
+ * <p>
  * Calcul la distance d'édition entre deux mots en considérant leur similarité
- * implantation de l'algorithme de Jaro-Winkler
+ * implantation de l'algorithme de Levenshtein
  */
 
 
 public class WordDistance {
 
-    public Double apply(String left, String right)throws IllegalArgumentException   {
-            final double defaultScalingFactor = 0.1;
-
-            if (left == null || right == null) {
-                throw new IllegalArgumentException("Strings must not be null");
-            }
-
-            final int[] mtp = matches(left, right);
-            final double m = mtp[0];
-            if (m == 0) {
-                return 0D;
-            }
-            final double j = ((m / left.length() + m / right.length() + (m - mtp[1]) / m)) / 3;
-            return j < 0.7D ? j : j + Math.min(defaultScalingFactor, 1D / mtp[3]) * mtp[2] * (1D - j);
-
+    public int Compare(String left, String right, int limite) {
+        if (left == null || right == null) {
+            throw new IllegalArgumentException("Strings must not be null");
+        }
+        if (limite < 0) {
+            throw new IllegalArgumentException("Threshold must not be negative");
         }
 
-        protected static int[] matches(String first, String second) {
-            CharSequence max, min;
-            if (first.length() > second.length()) {
-                max = first;
-                min = second;
-            } else {
-                max = second;
-                min = first;
+        int n = left.length();
+        int m = right.length();
+
+        // si une String est vide
+        if (n == 0) {
+            return m <= limite ? m : -1;
+        } else if (m == 0) {
+            return n <= limite ? n : -1;
+        }
+
+        int[] p = new int[n + 1]; // tableau des couts à jour
+        int[] d = new int[n + 1]; // tableau des couts courrant
+        int[] tempD; //tableau pour swapper les deux
+
+        // remplie le premier tableau
+        int boundary = Math.min(n, limite) + 1;
+        for (int i = 0; i < boundary; i++) {
+            p[i] = i;
+        }
+        Arrays.fill(p, boundary, p.length, Integer.MAX_VALUE);
+        Arrays.fill(d, Integer.MAX_VALUE);
+
+        //itérations
+        for (int j = 1; j <= m; j++) {
+            char rightJ = right.charAt(j - 1); // jth character of right
+            d[0] = j;
+
+            // calcule les indices
+            int min = Math.max(1, j - limite);
+            int max = j > Integer.MAX_VALUE - limite ? n : Math.min(
+                    n, j + limite);
+
+            if (min > max) {
+                return -1;
             }
-            final int range = Math.max(max.length() / 2 - 1, 0);
-            final int[] matchIndexes = new int[min.length()];
-            Arrays.fill(matchIndexes, -1);
-            final boolean[] matchFlags = new boolean[max.length()];
-            int matches = 0;
-            for (int mi = 0; mi < min.length(); mi++) {
-                final char c1 = min.charAt(mi);
-                for (int xi = Math.max(mi - range, 0), xn = Math.min(mi + range + 1, max.length()); xi < xn; xi++) {
-                    if (!matchFlags[xi] && c1 == max.charAt(xi)) {
-                        matchIndexes[mi] = xi;
-                        matchFlags[xi] = true;
-                        matches++;
-                        break;
-                    }
-                }
+
+            if (min > 1) {
+                d[min - 1] = Integer.MAX_VALUE;
             }
-            final char[] ms1 = new char[matches];
-            final char[] ms2 = new char[matches];
-            for (int i = 0, si = 0; i < min.length(); i++) {
-                if (matchIndexes[i] != -1) {
-                    ms1[si] = min.charAt(i);
-                    si++;
-                }
-            }
-            for (int i = 0, si = 0; i < max.length(); i++) {
-                if (matchFlags[i]) {
-                    ms2[si] = max.charAt(i);
-                    si++;
-                }
-            }
-            int transpositions = 0;
-            for (int mi = 0; mi < ms1.length; mi++) {
-                if (ms1[mi] != ms2[mi]) {
-                    transpositions++;
-                }
-            }
-            int prefix = 0;
-            for (int mi = 0; mi < min.length(); mi++) {
-                if (first.charAt(mi) == second.charAt(mi)) {
-                    prefix++;
+
+            // passe [min, max] en diagonale
+            for (int i = min; i <= max; i++) {
+                if (left.charAt(i - 1) == rightJ) {
+                    d[i] = p[i - 1];
                 } else {
-                    break;
+                    d[i] = 1 + Math.min(Math.min(d[i - 1], p[i]), p[i - 1]);
                 }
             }
-            return new int[]{matches, transpositions / 2, prefix, max.length()};
+
+            //swap les distances
+            tempD = p;
+            p = d;
+            d = tempD;
         }
+
+        if (p[n] <= limite) {
+            return p[n];
+        }
+        return -1;
     }
-
-
 }
+
+
